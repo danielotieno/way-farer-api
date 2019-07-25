dd905043e3a5e90c0e4b83d25eb9bddc828d6bc1
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-25 18:43:16 +0300

    [chore #167418063] Add create a trip route

--- app/routes/api/v1/index.js
+++ app/routes/api/v1/index.js
@@ -4,6 +4,10 @@ const apiPrefix = '/api/v1'
 
 const routes = app => {
   app.use(apiPrefix, trips)
+  app.use('*', (req, res) =>
+    res.status(404).json({ message: 'Kindly, check if your URL is correct' }),
+  )
+
   return app
 }
 

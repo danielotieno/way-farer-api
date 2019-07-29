4cb7ff6a75c47f847ca7d4f6d880351bfe3c5bc7
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-29 04:04:46 +0300

    [chore #167558065] Add test

--- app/test/__tests__/getSpecificTrip.spec.js
+++ app/test/__tests__/getSpecificTrip.spec.js
@@ -9,4 +9,22 @@ describe('Test get a specific Trip', () => {
     expect(JSON.parse(response.text).error).toEqual('trip not found')
     expect(response.status).toBe(404)
   })
+
+  // eslint-disable-next-line prettier/prettier
+  test('It should reponse the GET method with ID', async() => {
+    const payload = {
+      seating_capacity: 10,
+      bus_number: 'RAD 264 K',
+      origin: 'Mombasa',
+      destination: 'Kigali',
+      fare: 4600.0,
+    }
+    await request(app)
+      .post('/api/v1/trips')
+      .set('Content-Type', 'application/json')
+      .send(payload)
+    const response = await request(app).get('/api/v1/trips')
+    expect(JSON.parse(response.text).status).toBe('success')
+    expect(response.status).toBe(200)
+  })
 })

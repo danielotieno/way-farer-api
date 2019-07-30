0eea5f4a06651fa3493fee7cd182a861c10c1084
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-31 02:30:05 +0300

    [Fix #167418701] Fix space before function eslint error

--- app/test/__tests__/patchTrip.spec.js
+++ app/test/__tests__/patchTrip.spec.js
@@ -3,15 +3,13 @@ import app from '../../index'
 const request = require('supertest')
 
 describe('Test PATCH a specific Trip', () => {
-  // eslint-disable-next-line prettier/prettier
-  test('It should respond with trip not found when passing wrong id', async() => {
+  test('It should respond with trip not found when passing wrong id', async () => {
     const response = await request(app).patch('/api/v1/trips/1/cancel')
     expect(JSON.parse(response.text).error).toEqual('Trip not found')
     expect(response.status).toBe(404)
   })
 
-  // eslint-disable-next-line prettier/prettier
-  test('It should retrive back a cancelled trip', async() => {
+  test('It should retrive back a cancelled trip', async () => {
     const payload = {
       seatingCapacity: 24,
       busNumber: 'RAD 264 K',

a55b2d906d9d8b5f0fce506b31a5338d7d2f9646
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-30 12:41:24 +0300

    [chore #167590117] Add patch route

--- app/routes/api/v1/trips.js
+++ app/routes/api/v1/trips.js
@@ -6,5 +6,6 @@ const router = Router()
 router.post('/trips', TripsController.createTrip)
 router.get('/trips', TripsController.getAllTrips)
 router.get('/trips/:id', TripsController.getSingleTrip)
+router.patch('/trips/:id/cancel', TripsController.cancelTrip)
 
 export default router

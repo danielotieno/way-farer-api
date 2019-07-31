6b9d52d6c6fcc65803a18edbe1642322aa3ba526
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-31 11:08:34 +0300

    [chore #167418701] Test firstName is required

--- app/test/__tests__/auth.spec.js
+++ app/test/__tests__/auth.spec.js
@@ -19,4 +19,20 @@ describe('Test Authentication', () => {
     )
     expect(response.status).toBe(201)
   })
+
+  test('That first firstName is required when creating a user', async () => {
+    const payload = {
+      lastName: 'Otieno',
+      email: 'oti@gmail.com',
+      password: '123456789',
+    }
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .set('Content-Type', 'application/json')
+      .send(payload)
+    expect(JSON.parse(response.text).error[0].message).toEqual(
+      '"firstName" is required',
+    )
+    expect(response.status).toBe(400)
+  })
 })

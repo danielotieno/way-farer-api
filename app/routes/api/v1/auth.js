d93d68602714ff240e68566823575b3f80ba55f0
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-02 21:12:46 +0300

    [Fix #167418805] Fix admin route

--- app/routes/api/v1/auth.js
+++ app/routes/api/v1/auth.js
@@ -1,13 +1,12 @@
 import { Router } from 'express'
 import UserController from '../../../controller/UsersController'
-import AdminController from '../../../controller/AdminsController'
 
 const { localAuthentication } = require('../../../../config/passportConfig')
 
 const router = Router()
 
 router.post('/auth/signup', UserController.createUser)
-router.post('/auth/signup/admin', AdminController.createAdmin)
+router.post('/auth/signup/admin', UserController.createAdmin)
 router.post('/auth/login', localAuthentication, UserController.loginUser)
 
 export default router

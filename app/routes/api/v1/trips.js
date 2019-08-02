e89779e08611a9151c0aa4c2873b4d5504f38368
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-03 00:01:36 +0300

    [chore #167418805] Protect routes

--- app/routes/api/v1/trips.js
+++ app/routes/api/v1/trips.js
@@ -7,8 +7,13 @@ const { jwtAuthentication } = require('../../../../config/passportConfig')
 const router = Router()
 
 router.post('/trips', jwtAuthentication, isAdmin, TripsController.createTrip)
-router.get('/trips', TripsController.getAllTrips)
-router.get('/trips/:id', TripsController.getSingleTrip)
-router.patch('/trips/:id/cancel', TripsController.cancelTrip)
+router.get('/trips', jwtAuthentication, TripsController.getAllTrips)
+router.get('/trips/:id', jwtAuthentication, TripsController.getSingleTrip)
+router.patch(
+  '/trips/:id/cancel',
+  jwtAuthentication,
+  isAdmin,
+  TripsController.cancelTrip,
+)
 
 export default router

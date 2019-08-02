610f85c29d01e81160a9dba243de41688173b9b1
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-03 00:05:58 +0300

    [chore #167418805] Update routes

--- app/routes/api/v1/trips.js
+++ app/routes/api/v1/trips.js
@@ -1,19 +1,11 @@
 import { Router } from 'express'
 import TripsController from '../../../controller/TripsController'
-import isAdmin from '../../../../helpers/isAdmin'
-
-const { jwtAuthentication } = require('../../../../config/passportConfig')
 
 const router = Router()
 
-router.post('/trips', jwtAuthentication, isAdmin, TripsController.createTrip)
-router.get('/trips', jwtAuthentication, TripsController.getAllTrips)
-router.get('/trips/:id', jwtAuthentication, TripsController.getSingleTrip)
-router.patch(
-  '/trips/:id/cancel',
-  jwtAuthentication,
-  isAdmin,
-  TripsController.cancelTrip,
-)
+router.post('/trips', TripsController.createTrip)
+router.get('/trips', TripsController.getAllTrips)
+router.get('/trips/:id', TripsController.getSingleTrip)
+router.patch('/trips/:id/cancel', TripsController.cancelTrip)
 
 export default router

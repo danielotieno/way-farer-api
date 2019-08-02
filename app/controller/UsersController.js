59808e6976275f982ec250786668b9123b262d07
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-02 16:34:16 +0300

    [chore #167418712] Create login route

--- app/controller/UsersController.js
+++ app/controller/UsersController.js
@@ -1,7 +1,13 @@
 import UserModel from '../models/User'
 import EncryptData from '../../helpers/EncryptPassword'
+import createToken from '../../helpers/jwtToken'
 
-const { signupValidation } = require('../../helpers/validations')
+const { secretKey, jwtExpiration } = require('../../config/config')
+
+const {
+  signupValidation,
+  loginValidation,
+} = require('../../helpers/validations')
 
 class UserController {
   static async createUser(req, res) {
@@ -18,6 +24,22 @@ class UserController {
     req.body.password = EncryptData.generateHash(req.body.password)
     const message = await UserModel.createUser(req.body)
     return res.status(201).send({ status: 'success', message })
+  }
+
+  static async loginUser(req, res) {
+    // Validate fields before creating User
+    const { error } = loginValidation(req.body)
+    if (error)
+      return res.status(400).send({ status: 'error', error: error.details })
+
+    const token = createToken(
+      { id: req.user.id, role: req.user.role },
+      secretKey,
+      { expiresIn: jwtExpiration },
+    )
+    return res
+      .status(200)
+      .send({ status: 'success', message: 'LoggedIn successfully', token })
   }
 }
 
--- app/routes/api/v1/auth.js
+++ app/routes/api/v1/auth.js
@@ -4,5 +4,6 @@ import UserController from '../../../controller/UsersController'
 const router = Router()
 
 router.post('/auth/signup', UserController.createUser)
+router.post('/auth/login', UserController.loginUser)
 
 export default router

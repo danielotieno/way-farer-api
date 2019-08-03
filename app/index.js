ead3974fe343b4af847d3963efd12e5a86db2585
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-03 08:19:10 +0300

    [chore #167684502] Add Procfile

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
 import passport from 'passport'
-
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 import routes from './routes/api/v1'
 
 const app = express()
@@ -15,6 +16,7 @@ if (app.get('env') === 'development') {
   app.use(morgan('dev'))
 }
 app.use(passport.initialize())
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 routes(app)
 

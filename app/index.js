693355e19500275fe9a20861eae9aa429420da54
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-03 08:48:37 +0300

    [Fix #167684502] Fix swagger route

--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
 import passport from 'passport'
-import swaggerUi from 'swagger-ui-express'
-import * as swaggerDocument from './swagger.json'
 import routes from './routes/api/v1'
 
 const app = express()
@@ -16,7 +14,6 @@ if (app.get('env') === 'development') {
   app.use(morgan('dev'))
 }
 app.use(passport.initialize())
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 routes(app)
 

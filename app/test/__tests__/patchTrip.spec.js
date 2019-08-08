09e02cfffac82d025b2e5688417bed11a2e4b36e
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-08 12:37:09 +0300

    chore(patch trip):add tests for patching a trip
    [Delivers #167772710]

--- app/test/__tests__/patchTrip.spec.js
+++ app/test/__tests__/patchTrip.spec.js
@@ -1,10 +1,16 @@
+import request from 'supertest'
 import app from '../../index'
-
-const request = require('supertest')
+import getToken from '../testHelper'
 
 describe('Test PATCH a specific Trip', () => {
+  let token
+  beforeEach(async () => {
+    token = await getToken()
+  })
   test('It should respond with trip not found when passing wrong id', async () => {
-    const response = await request(app).patch('/api/v1/trips/1/cancel')
+    const response = await request(app)
+      .patch('/api/v1/trips/1/cancel')
+      .set('Authorization', `Bearer ${token}`)
     expect(JSON.parse(response.text).error).toEqual('Trip not found')
     expect(response.status).toBe(404)
   })
@@ -19,11 +25,12 @@ describe('Test PATCH a specific Trip', () => {
     }
     const { body } = await request(app)
       .post('/api/v1/trips')
+      .set('Authorization', `Bearer ${token}`)
       .set('Content-Type', 'application/json')
       .send(payload)
-    const response = await request(app).patch(
-      `/api/v1/trips/${body.data.tripId}/cancel`,
-    )
+    const response = await request(app)
+      .patch(`/api/v1/trips/${body.data.tripId}/cancel`)
+      .set('Authorization', `Bearer ${token}`)
     expect(JSON.parse(response.text).status).toEqual(200)
     expect(JSON.parse(response.text).message).toEqual(
       'Trip cancelled successfully',

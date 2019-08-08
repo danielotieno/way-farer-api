9830273512275f18aa4aeea8665535c86fd6d6ca
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-08 15:13:57 +0300

    chore(delete):add test when booking is deleted
    [Delivers #167772710]

--- app/test/__tests__/deleteBooking.spec.js
+++ app/test/__tests__/deleteBooking.spec.js
@@ -1,12 +1,51 @@
+import request from 'supertest'
+import { wait } from '@hapi/hoek'
 import app from '../../index'
-
-const request = require('supertest')
+import getToken from '../testHelper'
 
 describe('Test delete a booking', () => {
+  let token
+  beforeEach(async () => {
+    token = await getToken()
+  })
   test('It should respond with not found when passing wrong id', async () => {
-    const response = await request(app).delete('/api/v1/bookings/1')
+    const response = await request(app)
+      .delete('/api/v1/bookings/1')
+      .set('Authorization', `Bearer ${token}`)
     expect(JSON.parse(response.text).status).toEqual(404)
     expect(JSON.parse(response.text).error).toEqual('Booking not found')
     expect(response.status).toBe(404)
+  })
+
+  test('It should be able to delete all bookings', async () => {
+    const payload = {
+      seatingCapacity: 24,
+      busNumber: 'RAD 254 J',
+      origin: 'Mombasa',
+      destination: 'Kigali',
+      fare: 4500.0,
+    }
+    const response = await request(app)
+      .post('/api/v1/trips')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .send(payload)
+    const result = JSON.parse(response.text)
+    const { tripId } = result.data
+
+    const booking = {
+      tripId,
+      numberOfSeats: 4,
+    }
+    const { body } = await request(app)
+      .post('/api/v1/bookings')
+      .set('Authorization', `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .send(booking)
+    const res = await request(app)
+      .delete(`/api/v1/bookings/${body.data.bookingId}`)
+      .set('Authorization', `Bearer ${token}`)
+    console.log(res)
+    expect(res.status).toBe(204)
   })
 })

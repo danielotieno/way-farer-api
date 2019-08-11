9912e1242b80d648efc82bc296797f1e1e1da2f9
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-11 12:46:21 +0300

    chore(database):add database variables in the config file
    [Delivers #167826621]

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,13 @@ const envVarsSchema = Joi.object({
     .allow(['development', 'production', 'test'])
     .required(),
   PORT: Joi.number().default(8080),
+  DATABASE: Joi.string().required(),
+  TEST_DB: Joi.string().default('wayfarer_test_db'),
+  DATABASE_DIALECT: Joi.string().default('postgres'),
+  DATABASE_PASSWORD: Joi.string().default(null),
+  DATABASE_USER: Joi.string().required(),
+  DATABASE_URL: Joi.string().default(null),
+  HOST: Joi.string().required(),
   SECRET_KEY: Joi.string().required(),
   JWT_EXPIRATION: Joi.string().required(),
 })
@@ -24,6 +31,13 @@ if (error) {
 const config = {
   env: envVars.NODE_ENV || 'development',
   port: envVars.PORT,
+  databaseName: envVars.DATABASE,
+  testDbName: envVars.TEST_DB,
+  dbUsername: envVars.DATABASE_USER,
+  databaseDialect: envVars.DATABASE_DIALECT,
+  dbPassword: envVars.DATABASE_PASSWORD,
+  databaseUrl: envVars.DATABASE_URL,
+  host: envVars.HOST,
   secretKey: envVars.SECRET_KEY,
   jwtExpiration: envVars.JWT_EXPIRATION,
 }

cc35647ff3086193d861c6b25673bd58d2509ecb
Daniel Otieno <danielotieno.ke@gmail.com>
2019-08-12 12:05:46 +0300

    chore(database table):create database table form trips and booking
    [Delivers #167826621]

--- server/database/createTable.js
+++ server/database/createTable.js
@@ -9,7 +9,7 @@ const createTables = async () => {
     email VARCHAR(250) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
     role VARCHAR(50) NOT NULL DEFAULT 'user',
-    created_on TIMESTAMP
+    created_on TIMESTAMP with time zone DEFAULT now()
 );`
   const tripTable = `CREATE TABLE IF NOT EXISTS trips (
     trip_id SERIAL PRIMARY KEY,
@@ -21,10 +21,20 @@ const createTables = async () => {
     fare NUMERIC NOT NULL,
     status VARCHAR(50) NOT NULL DEFAULT 'active',
 );`
+  const bookingTable = `CREATE TABLE IF NOT EXISTS bookings (
+    booking_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    trip_id INTEGER REFERENCES trips(trip_id),
+    number_of_seat INTEGER NOT NULL
+,
+
+);`
   try {
     await db.none(userTable)
     logger.info('User Table Created')
     await db.none(tripTable)
+    logger.info('Trip Table Created')
+    await db.none(bookingTable)
     logger.info('Trip Table Created')
   } catch (error) {
     logger.info('error occurred while creating tables', error)
